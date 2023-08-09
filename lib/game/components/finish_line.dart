24e5f2bcf1fd35880b28d407e13b646d806949f4
rafaelbarbsoatec <rafaelbarbsoatec@gmail.com>
2023-08-09 15:08:25 -0300

    adiciona linha de chegada

--- lib/game/components/finish_line.dart
+++ lib/game/components/finish_line.dart
@@ -1,6 +1,5 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:npc_neural/game/components/finish_line_tile.dart';
-import 'package:npc_neural/game/components/knight.dart';
 import 'package:npc_neural/game/npc_neural_game.dart';
 import 'package:npc_neural/util/spritesheet.dart';
 
@@ -12,7 +11,7 @@ class FinishLine extends GameDecoration {
   });
 
   @override
-  Future<void> onLoad() async{
+  Future<void> onLoad() async {
     int countTiles = size.y ~/ NpcNeuralGame.tilesize;
     final sp = await Spritesheet.finishLine;
     final spInv = await Spritesheet.finishLineInverted;
@@ -22,20 +21,12 @@ class FinishLine extends GameDecoration {
         add(
           FinishLineTile(
             position: Vector2(0, NpcNeuralGame.tilesize * index),
-            sprite: index%2 == 0 ? sp : spInv,
+            sprite: index % 2 == 0 ? sp : spInv,
           ),
         );
       },
     );
     add(hitbox = RectangleHitbox(size: size));
     return super.onLoad();
-  }
-
-  @override
-  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
-    if (other is Knight) {
-      Spritesheet.chestOpen.then((value) => setAnimation(value));
-    }
-    super.onCollision(intersectionPoints, other);
   }
 }
