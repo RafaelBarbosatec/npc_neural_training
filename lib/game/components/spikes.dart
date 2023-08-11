3d5c79a34e415ddc42d1090720d2ed35ba8eb2c0
rafaelbarbsoatec <rafaelbarbsoatec@gmail.com>
2023-08-11 12:03:02 -0300

    adiciona fit

--- lib/game/components/spikes.dart
+++ lib/game/components/spikes.dart
@@ -1,4 +1,5 @@
 import 'package:bonfire/bonfire.dart';
+import 'package:npc_neural/game/npc_neural_game.dart';
 import 'package:npc_neural/util/spritesheet.dart';
 
 class Spikes extends GameDecoration with DragGesture {
@@ -13,5 +14,29 @@ class Spikes extends GameDecoration with DragGesture {
   Future<void> onLoad() {
     add(RectangleHitbox(size: size, isSolid: true));
     return super.onLoad();
+  }
+
+  @override
+  void onEndDrag(int pointer) {
+    double restX = x % NpcNeuralGame.tilesize;
+    double restY = y % NpcNeuralGame.tilesize;
+    if (restX < NpcNeuralGame.tilesize / 2) {
+      restX = -restX;
+    } else {
+      restX = NpcNeuralGame.tilesize - restX;
+    }
+
+    if (restY < NpcNeuralGame.tilesize / 2) {
+      restY = -restY;
+    } else {
+      restY = NpcNeuralGame.tilesize - restY;
+    }
+
+    position += Vector2(
+      restX,
+      restY,
+    );
+
+    super.onCancelDrag(pointer);
   }
 }
